1912506667976960ac9c223512df3a085fbae3eb
Amy Cen <amy.cen@blackrock.com>
2019-06-26 20:07:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ let driver = {};
 function updateDriverWithKeyAndValue(driver, key, value){
   return Object.assign({}, {key: value})
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  return driver[key] = value;
+}
+
